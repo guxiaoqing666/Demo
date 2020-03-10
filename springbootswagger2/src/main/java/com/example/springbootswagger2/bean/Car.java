620055336d505e2a3595bc9c207db4fe662b76c6
guxiaoqing666 <1348053651@qq.com>
2020-03-10 17:52:06 +0800

    update

---
+++ springbootswagger2/src/main/java/com/example/springbootswagger2/bean/Car.java
@@ -0,0 +1,19 @@
+package com.example.springbootswagger2.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by smlz on 2019/5/5.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    private String name;
+
+
+}
