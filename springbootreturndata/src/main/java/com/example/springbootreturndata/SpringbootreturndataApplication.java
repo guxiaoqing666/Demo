0781e15d707c22d32df71e95d044a2e706323a7e
guxiaoqing666 <1348053651@qq.com>
2020-03-10 15:46:22 +0800

    update

---
+++ springbootreturndata/src/main/java/com/example/springbootreturndata/SpringbootreturndataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreturndata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootreturndataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootreturndataApplication.class, args);
+    }
+
+}
---
+++ springbootreturndata/src/test/java/com/example/springbootreturndata/SpringbootreturndataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreturndata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootreturndataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
